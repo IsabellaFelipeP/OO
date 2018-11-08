192e6f12297312e6e126d4ed026fef352fb62ff0
Isabella Palmieri <isabellafelipe@hotmail.com>
2018-11-08 16:44:49 -0400

    Initial commit

---
+++ Fase 1/Cliente.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class Cliente
+{
+    private $nome;
+    private $cpf;
+    private $endereco;
+q
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+    }
+
+    public function Dados()
+    {
+        $dados = array(1 => 'Joao', '02234578923', 'Florida Branca',
+            2 => 'Maria', '00123478911', 'Mar Branca',
+            3 => 'Carlos', '00345672189', 'Pedra Branca');
+    }
+
+
+
+
+
+}
---
+++ Fase 1/Index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="stylesheet" type="text/css" href="Marcador.css">
+    <title> Cadastro de Cliente</title>
+</head>
+
+<body>
+    <div class="container ">
+        <div class="row ">
+            <div class = "col-md-12 Marcador">
+            <h1> Cadastro de Cliente</h1>
+            </div>
+         </div>
+
+        <form>
+            
+               <table class = ".table-bordered">
+                <br>   <div class = mod-md-3>
+                <label for="Nome">Nome:</label>
+                <input type="Nome" class="form-control" id="Nome">
+
+                </div>
+
+
+
+            </table>
+
+
+            </div>
+
+
+        </form>
+    </div>
+<?php
+
+
+//require_once "Cliente.php";
+/*
+$dados = array(1 => 'Joao', '02234578923', 'Florida Branca',
+               2 => 'Maria', '00123478911', 'Mar Branca',
+               3 => 'Carlos', '00345672189', 'Pedra Branca');
+
+var_dump($dados);
+*/
+?>
+
+</body>
+</html>
+
